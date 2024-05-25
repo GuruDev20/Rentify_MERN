3283a1f1756c22e06250020ba33544efcb7c5349
Dev <guru01803@gmail.com>
2024-05-25 10:46:15 +0530

    updated setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,35 @@
 import React from 'react'
+import './Styles/App.css'
+import {Routes,Route} from 'react-router-dom'
+import Welcome from './Components/Welcome';
 function App() {
+    window.addEventListener("scroll", reveal);
+    function reveal() {
+        var reveals = document.querySelectorAll(".reveal");
+        var rev = document.querySelectorAll(".rev");
+        var revv = document.querySelectorAll(".revv");
+        for (var i = 0; i < reveals.length; i++) {
+            var windowheight = window.innerHeight;
+            var revealtop = reveals[i].getBoundingClientRect().top;
+            var revealpoint = 100;
+            if (reveals[i] && revealtop < windowheight - revealpoint) {
+                reveals[i].classList.add("active");
+            } else if (reveals[i]) {
+                reveals[i].classList.remove("active");
+            }
+            if (rev[i] && revv[i] && revealtop < windowheight - revealpoint) {
+                rev[i].classList.add("active");
+                revv[i].classList.add("active");
+            } else if (rev[i] && revv[i]) {
+                rev[i].classList.remove("active");
+                revv[i].classList.remove("active");
+            }
+        }
+    }
     return (
-        <div>App</div>
+        <Routes>
+            <Route path='/' element={<Welcome/>}/>
+        </Routes>
     )
 }
 
---
+++ client/src/Components/Welcome.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Navbar from './Navbar'
+export default function Welcome() {
+    return (
+        <div>
+            <Navbar/>
+        </div>
+    )
+}
